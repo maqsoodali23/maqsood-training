f50ae7f56d8482cc30cbcdbe4ece9a2673f45746
Maqsood-invozone <apple@apples-mbp.home>
2021-01-14 10:07:37 +0500

    Http routes pracice code

---
+++ macInformation.js
@@ -0,0 +1,25 @@
+require ('dotenv').config();
+const os = require ('os');
+const fs = require ('fs');
+
+let cpuinfo = os.cpus();
+let homedirectory = os.homedir();
+let osPlatform = os.platform();
+let architecture = os.arch();
+let release = os.release();
+
+let allInfo = {
+    osPlatform,
+    architecture,
+    homedirectory,
+    release,
+    cpuinfo,
+};
+
+fs.writeFile((homedirectory + '/Desktop/' + process.env.FILE_NAME), JSON.stringify(allInfo), (error) => {
+    if(error) throw error;
+    console.log('File created successfully!');
+});
+
+// console.log(homedirectory);
+// console.log(JSON.stringify(allInfo));
